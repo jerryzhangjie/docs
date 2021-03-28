c3ff9af69c88120f4e36429d7ae9d093c419b196
Zhang-Jerry <z_jerry2016@163.com>
2021-03-28 21:04:52 +0800

    add

--- docs/.vuepress/config/navConfig.js
+++ docs/.vuepress/config/navConfig.js
@@ -2,9 +2,9 @@ module.exports = [
   {text: '首页', link: '/'},
   {text: '关于', link: '/about/'},
   {text: '技术', items: [
-    {text: '前端', link: '/technology/front_end/'},
-    {text: '服务端', link: '/technology/server_end/'},
-    {text: '其他', link: '/technology/other/'}
+    {text: '前端', link: '/technology/1. 前端/'},
+    {text: '服务端', link: '/technology/2. 服务端/'},
+    {text: '其他', link: '/technology/3. 其它/'}
   ]},
   {text: '生活杂记', link: '/life/'},
   {text: '其他', link: '/other/'},
--- docs/.vuepress/config/sidebarConfig.js
+++ docs/.vuepress/config/sidebarConfig.js
@@ -1,35 +1,94 @@
 const fs = require('fs')
 const path = require('path')
+const sidebarConfig = {}
 
 /**
- * @param filePath 需要遍历的文件路径
+ * 获得目标文件夹绝对路径
+ * @param dirPath 目标文件夹相对路径
+ * @returns 
  */
-function getSidebarList(filePath){
+function getFullDirPath(dirPath) {
+    return path.resolve(__dirname, `../../${dirPath}`)
+}
+
+
+
+/**
+ * 获得目标文件夹的所有元素
+ * @param dirPath 目标文件夹相对路径
+ */
+function getAllDir(dirPath) {
+    const fullDirPath = getFullDirPath(dirPath)
+    let allDirs = fs.readdirSync(fullDirPath)
+    allDirs = allDirs.filter((dir) => {
+        return dir !== '.DS_Store' && dir !== 'README.md'
+    }).sort((a, b) => {
+        let l = parseFloat(a.split('.')[0])
+        let r = parseFloat(b.split('.')[0])
+        if (l < r) {
+            return -1
+        } else if (l > r) {
+            return 1
+        } else {
+            const l_i = a.indexOf('.')
+            const l_j = a.indexOf(' ')
+            const r_i = b.indexOf('.')
+            const r_j = b.indexOf(' ')
+            l = parseFloat(a.substring(l_i + 1, l_j))
+            r = parseFloat(b.substring(r_i + 1, r_j))
+            if (l < r) {
+                return -1
+            } else {
+                return 1
+            }
+        }
+    })
+    return allDirs
+}
+
+/**
+ * @param dirPath 需要遍历的文件路径
+ */
+function getSidebarList(dirPath){
     const sidebarList = []
-    const fullFilePath = path.resolve(__dirname, `../../${filePath}`)
-    const files = fs.readdirSync(fullFilePath)
+    const files = getAllDir(dirPath)
     files.forEach((file) => {
         if (!file.includes('.md')) return
         const fileName = file.replace('.md', '')
-        sidebarList.push([encodeURI(filePath + fileName), fileName])
+        sidebarList.push([`${dirPath}/${fileName}`, fileName])
     })
     return sidebarList
 }
 
-module.exports = {
-    '/technology/front_end/': [
-        {
-            title: '浏览器',        // 必要
-            // path: '/',          // 可选, 标题的跳转链接，应为绝对路径，与 collapsable: true 互斥
-            collapsable: true,     // 可选, 是否可折叠
-            sidebarDepth: 1,       // 可选的, 展示标题深度
-            children: getSidebarList('/technology/front_end/browser/')
-        },
-        {
-            title: 'ECMAScript',
-            collapsable: true,
-            sidebarDepth: 1,
-            children: getSidebarList('/technology/front_end/ecma_script/')
-        },
-    ]
+/**
+ * 设置 sidebarConfig
+ * @param dirPath 目标文件夹相对路径
+ * @param type 1-侧边栏分组 2-不分组
+ */
+function setSidebarConfig(dirPath, type) {
+    if (type === '1') {
+        const allDirs = getAllDir(dirPath)
+        allDirs.forEach((dir) => {
+            const path = `${dirPath}/${dir}`
+            sidebarConfig[path] = []
+            let allSubDirs = getAllDir(path)
+            allSubDirs.forEach((subDir) => {
+                const subPath = `${path}/${subDir}`
+                const sidebarList = getSidebarList(subPath, path)
+                sidebarConfig[path].push(
+                    {
+                        title: subDir,        // 必要
+                        // path: '/',          // 可选, 标题的跳转链接，应为绝对路径，与 collapsable: true 互斥
+                        collapsable: true,     // 可选, 是否可折叠
+                        sidebarDepth: 1,       // 可选的, 展示标题深度
+                        children: sidebarList
+                    }
+                )
+            })
+        })
+    }
 }
+
+setSidebarConfig('/technology', '1')
+
+module.exports = sidebarConfig
